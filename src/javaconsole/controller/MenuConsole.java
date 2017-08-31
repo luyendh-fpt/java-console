f396a7de9f84ed5f8fc43629f577b35ce5b92aba
Xuan Hung Dao <luyendh@nal.vn>
2017-08-31 10:12:42 +0700

    Xử lý tạo menu.

---
+++ src/javaconsole/controller/MenuConsole.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaconsole.controller;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author daolinh
+ */
+public class MenuConsole {
+
+    public static void main(String[] args) {
+        MenuConsole menu = new MenuConsole();
+        menu.createMenu();
+    }
+
+    public void createMenu() {
+        while (true) {
+            System.out.println("=========Student Manager========");
+            System.out.println("1. Student list.");
+            System.out.println("2. Add student.");
+            System.out.println("3. Edit student.");
+            System.out.println("4. Delete student.");
+            System.out.println("5. Exit.");
+            System.out.println("=================================");
+            System.out.println("Please enter your choice: ");
+            Scanner scanner = new Scanner(System.in);
+            // Yêu cầu người dùng nhập chuỗi ký tự, gán giá trị người dùng nhập vào
+            // ra biến kiểu  chuỗi tên là strChoice.
+            String strChoice = scanner.nextLine();
+            System.out.println(strChoice);
+            // Kiểm tra dữ liệu người dùng nhập vào có là số hay không ?
+            // Trong trường hợp không phải là số thì thông báo cho người dùng và bắt đầu lại vòng lặp.
+            int choice = 0;
+            try {
+                // Ép kiểu của biến strChoice về int.
+                choice = Integer.parseInt(strChoice);
+                System.out.println("Choice: " + choice);
+            } catch (java.lang.NumberFormatException e) {
+                // Cần có phần lưu log lỗi ở đây.
+                System.err.println("Please enter a number.");
+                continue;
+            }
+
+            if (choice == 5) {
+                break;
+            } else {
+                switch (choice) {
+                    case 1:
+                        // Do something. Please do something.
+                        System.out.println("Print list of student.");
+                        break;
+                    case 2:
+                        // Do something. Please do something.
+                        System.out.println("Add new student.");
+                        break;
+                    case 3:
+                        // Do something. Please do something.
+                        System.out.println("Edit student.");
+                        break;
+                    case 4:
+                        // Do something. Please do something.
+                        System.out.println("Delete student.");
+                        break;
+                    default:
+                        // Do something. Please do something.
+                        System.out.println("Please enter number from 1 to 5.");
+                        break;
+                }
+            }
+        }
+    }
+
+}
---
+++ src/javaconsole/entity/Student.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaconsole.entity;
+
+/**
+ *
+ * @author daolinh
+ */
+public class Student {
+
+    private long id;
+    private String name;
+    private String email;
+    private String birthday;
+    private String phone;
+    private String address;
+    private String rollNumber;
+    private String className;
+
+    public Student() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getRollNumber() {
+        return rollNumber;
+    }
+
+    public void setRollNumber(String rollNumber) {
+        this.rollNumber = rollNumber;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+}
